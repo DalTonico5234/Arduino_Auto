5971928743656a913ae82bef5eba1e5bd2081a3a
Leonardo Roxo <lcoroxo@gmail.com>
2025-03-08 12:27:28 -0300

    primeira versão

---
+++ Arduino_Auto.ino
@@ -0,0 +1,186 @@
+// Basic demo for accelerometer readings from Adafruit MPU6050
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TMPL2igSWEv0z"
+#define BLYNK_TEMPLATE_NAME "Quickstart Device"
+#define BLYNK_AUTH_TOKEN "4a80gtBmqu1YbQ10mDbAVDO1LK5C2Tj9"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+const int DIR = 12;
+const int STEP = 14;
+const int steps_per_rev = 200;
+
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+// char ssid[] = "Teste Pic";
+// char pass[] = "agoravai";
+
+BlynkTimer timer;
+
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V4)
+{
+  digitalWrite(DIR, HIGH);
+  Serial.println("Spinning Clockwise...");
+
+  for (int i = 0; i < steps_per_rev; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(5000);
+  }
+}
+BLYNK_WRITE(V5)
+{
+  digitalWrite(DIR, LOW);
+  Serial.println("Spinning Anti-Clockwise...");
+
+  for (int i = 0; i < steps_per_rev; i++)
+  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(STEP, LOW);
+    delayMicroseconds(5000);
+  }
+}
+
+// This function is called every time the device is connected to the Blynk.Cloud
+BLYNK_CONNECTED()
+{
+  // Change Web Link Button message to "Congratulations!"
+  Serial.println("CONECTOU");
+}
+
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V2, millis() / 1000);
+}
+
+Adafruit_MPU6050 mpu;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+  //  You can also specify server:
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+  pinMode(STEP, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin())
+  {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+
+  Serial.println("");
+  delay(100);
+}
+// void balance(a, g, temp){
+
+// if(g.acceleration.x>0){
+//  digitalWrite(DIR, LOW);
+// for (int i = 0; i < steps_per_rev; i++)
+//{
+// digitalWrite(STEP, HIGH);
+// delayMicroseconds(5000);
+// digitalWrite(STEP, LOW);
+//  delayMicroseconds(5000);
+//}
+//}else if(g.acceleration.x<-2){
+// digitalWrite(DIR, HIGH);
+// for (int i = 0; i < steps_per_rev; i++)
+//{
+// digitalWrite(STEP, HIGH);
+// delayMicroseconds(5000);
+// digitalWrite(STEP, LOW);
+// delayMicroseconds(5000);
+//}
+//}
+//}
+void loop()
+{
+
+  /* Get new sensor events with the readings */
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  Serial.print("pos X: ");
+  Serial.print(a.acceleration.x);
+  Serial.println("");
+  Serial.print("pos z: ");
+  Serial.print(a.acceleration.z);
+  Serial.println("");
+  Serial.print("pos y: ");
+  Serial.print(a.acceleration.y);
+  Serial.println("");
+
+  // balance();
+
+  if (a.acceleration.z < -2)
+  {
+    digitalWrite(DIR, LOW);
+    // while (g.acceleration.x> -8)
+    //{
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(STEP, LOW);
+
+    //}
+  }
+  else if (a.acceleration.z > 2)
+  {
+    digitalWrite(DIR, HIGH);
+    // while (g.acceleration.x<-11)
+    //  {
+    digitalWrite(STEP, HIGH);
+    delayMicroseconds(5000);
+    digitalWrite(STEP, LOW);
+
+    //}
+  }
+
+  Blynk.run();
+  timer.run();
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Arduino_Auto
-Projeto desenvolvido para a disciplina de PIC-1 do curso de Engenharia de Computação da UFES. Consiste em um robô de duas rodas, projetado para se equilibrar automaticamente por meio de ajustes precisos realizados por um motor de passo, garantindo estabilidade e eficiência no seu funcionamento.
+##Resumo
+Projeto desenvolvido para a disciplina de PIC-1 do curso de Engenharia de Computação da UFES.\
+Consiste em um robô de duas rodas, projetado para se equilibrar automaticamente por meio de ajustes precisos realizados por um motor de passo, garantindo estabilidade e eficiência no seu funcionamento.\
+
+##Estrutura
+Sua estrutura é constituída por duas rodas ligadas a dois motores de passo Nema 17, que são ligados à uma placa de mdf de 16x11.5 cm. A placa tem quatro furos que permitem a passagem de 4 fusos trapezoidais, que a conecta com duas placas de mdf de mesmo tamanho situadas acima da primeira.\
+Assim, a estrutura do carrinho se assemelha a uma torre, na qual são conectados todos os componentes.\
+Utiliza um Acelerômetro e Giroscópio 3 Eixos MPU-6050 para detectar a inclinação da torre em relação à horizontal, fornecendo a base para os cálculos do ajuste do motor. \
+Utiliza um Driver Motor de Passo A4988 para gerenciar simultaneamente ambos os motores.\
+Seu código é executado por uma placa ESP32S NodeMcu ESP-12, que faz todos os cálculos de ajuste, assim como a maioria das conexões.\
+A placa recebe uma tensão de entrada de 5V, obtida usando uma bateria alcalina de 9V e um Regulador de Tensão Step Down LM2596.\
+Ambos os motores recebem a alimentação de outra bateria alcalina de 9V, conectada ao driver.\
+
+##Código
+O código do projeto inclui as bibliotecas da Adafruit para o MPU-6050 e para sensores gerais. Também inclui as bibliotecas de conexão Wire, Wifi, WifiClient e a biblioteca do aplicativo Blynk BlynkSimpleEsp32.\
+Se inicia no setup, definindo a taxa de comunicação serial e realizando a conexão com o aplicativo personalizável Blynk, responsável por receber e enviar todos os comandos remotos ao robô. Além disso, ele define as portas que serão usadas, além de configurar algumas funções de bibliotecas que serão usadas no loop principal. Também realiza a conexão e inicialização do acelerômetro e giroscópio.\
+No loop principal ocorre a chamada das funções que obtêm a inclinação do carrinho. Também são executadas as funções de balanceamento, usando como parâmetro o eixo Z do MPU-6050, e as de recebimento de comandos enviados via Blynk.\
