8316ba1a142cc21fb0cadc9e3bc69fabc132cce5
Leonardo Roxo <lcoroxo@gmail.com>
2025-03-17 23:17:58 -0300

    Corrigindo comentários

--- Arduino_Auto.ino
+++ Arduino_Auto.ino
@@ -1,4 +1,3 @@
-// Basic demo for accelerometer readings from Adafruit MPU6050
 #define BLYNK_TEMPLATE_ID           "TMPL2igSWEv0z"
 #define BLYNK_TEMPLATE_NAME         "Quickstart Device"
 #define BLYNK_AUTH_TOKEN            "4a80gtBmqu1YbQ10mDbAVDO1LK5C2Tj9"
@@ -17,31 +16,13 @@ const int steps_per_rev = 200;
 #include <BlynkSimpleEsp32.h>
 
 
-/*************************************************************
-
-  This is a simple demo of sending and receiving some data.
-  Be sure to check out other examples!
- *************************************************************/
-
-/* Fill-in information from Blynk Device Info here */
-
-/* Comment this out to disable prints and save space */
-
-
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-//char ssid[] = "Teste Pic";
-//char pass[] = "agoravai";
-
-char ssid[] = "S23NET5234";
-char pass[] = "MORREUFESPRAGA";
-
-
+// Credenciais para conexão via wifi
+char ssid[] = "NOME_DA_REDE";
+char pass[] = "SENHA_DA_REDE";
 
 BlynkTimer timer;
 
-// This function is called every time the Virtual Pin 0 state changes
+// Gira o motor no sentido horário
 BLYNK_WRITE(V4)
 {
   digitalWrite(DIR, HIGH);
@@ -55,6 +36,8 @@ BLYNK_WRITE(V4)
     delayMicroseconds(10000);
   }
 }
+
+// Gira o motor no sentido anti-horário
 BLYNK_WRITE(V5)
 {
   digitalWrite(DIR, LOW);
@@ -69,18 +52,15 @@ BLYNK_WRITE(V5)
   }
 }
 
-// This function is called every time the device is connected to the Blynk.Cloud
+// Exibe a mensagem de sucesso na conexão com o blynk
 BLYNK_CONNECTED()
 {
-  // Change Web Link Button message to "Congratulations!"
   Serial.println("CONECTOU");
 }
 
-// This function sends Arduino's uptime every second to Virtual Pin 2.
+// Envia o tempo de atividade da placa a cada segundo para o pino V2. 
 void myTimerEvent()
 {
-  // You can send any value at any time.
-  // Please don't send more that 10 values per second.
   Blynk.virtualWrite(V2, millis() / 1000);
 }
 
@@ -90,20 +70,14 @@ void setup(void)
 {
   Serial.begin(115200);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  //  You can also specify server:
-  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
-  // Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
 
-  // Setup a function to be called every second
   timer.setInterval(1000L, myTimerEvent);
   pinMode(STEP, OUTPUT);
   pinMode(DIR, OUTPUT);
   while (!Serial)
-    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+    delay(10); 
 
-  Serial.println("Adafruit MPU6050 test!");
-
-  // Try to initialize!
+  // Verifica se a MPU está devidamente conectada
   if (!mpu.begin())
   {
     Serial.println("Failed to find MPU6050 chip");
@@ -126,35 +100,15 @@ void setup(void)
   Serial.println("");
   delay(100);
 }
-// void balance(a, g, temp){
 
-// if(g.acceleration.x>0){
-//  digitalWrite(DIR, LOW);
-// for (int i = 0; i < steps_per_rev; i++)
-//{
-// digitalWrite(STEP, HIGH);
-// delayMicroseconds(5000);
-// digitalWrite(STEP, LOW);
-//  delayMicroseconds(5000);
-//}
-//}else if(g.acceleration.x<-2){
-// digitalWrite(DIR, HIGH);
-// for (int i = 0; i < steps_per_rev; i++)
-//{
-// digitalWrite(STEP, HIGH);
-// delayMicroseconds(5000);
-// digitalWrite(STEP, LOW);
-// delayMicroseconds(5000);
-//}
-//}
-//}
 void loop()
 {
 
-  /* Get new sensor events with the readings */
+  // Obtem os valores da MPU
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
+  // Imprime os valores lidos pela MPU
   Serial.print("pos X: ");
   Serial.print(a.acceleration.x);
   Serial.println("");
@@ -164,25 +118,6 @@ void loop()
   Serial.print("pos y: ");
   Serial.print(a.acceleration.y);
   Serial.println("");
-
-  // balance();
-
-  //if (a.acceleration.z < -2)
-  //{
-   // digitalWrite(DIR, LOW);
-   // digitalWrite(STEP, HIGH);
-   // delayMicroseconds(10000);
-   // digitalWrite(STEP, LOW);
-
-  //}
-  //else if (a.acceleration.z > 2)
-  //{
-   // digitalWrite(DIR, HIGH);
-   // digitalWrite(STEP, HIGH);
-    //delayMicroseconds(10000);
-   // digitalWrite(STEP, LOW);
-
-  //}
 
   Blynk.run();
   timer.run();
